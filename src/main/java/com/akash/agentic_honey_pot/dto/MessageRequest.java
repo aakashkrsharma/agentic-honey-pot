eb4c462e98bf2e40860d5644a7d471c3eebb24a6
Akash Sharma <aakashkrsharma9070@gmail.com>
2026-02-04 12:09:44 +0530

    modified message request DTO as per the hackathon message request format

--- src/main/java/com/akash/agentic_honey_pot/dto/MessageRequest.java
+++ src/main/java/com/akash/agentic_honey_pot/dto/MessageRequest.java
@@ -1,13 +1,31 @@
 package com.akash.agentic_honey_pot.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
 
 @Getter
+@Setter
 public class MessageRequest {
 
-    @JsonProperty("conversation_id")
-    private String conversationId;
+    private String sessionId;
+    private MessagePayload message;
+    private List<Object> conversationHistory;
+    private Metadata metadata;
 
-    private String message;
+    @Getter
+    @Setter
+    public static class MessagePayload {
+        private String sender;
+        private String text;
+        private long timestamp;
+    }
+
+    @Getter
+    @Setter
+    public static class Metadata {
+        private String channel;
+        private String language;
+        private String locale;
+    }
 }
