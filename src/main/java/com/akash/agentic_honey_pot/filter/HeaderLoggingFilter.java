3e7be596e6ff5592162e87a1cc2ab4d9998293f3
Akash Sharma <aakashkrsharma9070@gmail.com>
2026-02-04 20:31:58 +0530

    Adding readme file and code comments to service class for proper understanding

---
+++ src/main/java/com/akash/agentic_honey_pot/filter/HeaderLoggingFilter.java
@@ -0,0 +1,46 @@
+package com.akash.agentic_honey_pot.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class HeaderLoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(HeaderLoggingFilter.class);
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        log.info("=== INCOMING REQUEST ===");
+        log.info("Method: {}", request.getMethod());
+        log.info("URI: {}", request.getRequestURI());
+        log.info("Content-Type: {}", request.getContentType());
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                log.info("Header {} : {}", name, request.getHeader(name));
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
