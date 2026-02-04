a85bd59dc4ac8c7f6bd91face7b9c3d8b07e5fd0
Akash Sharma <aakashkrsharma9070@gmail.com>
2026-02-04 11:32:19 +0530

    Adding logger to log the request format

---
+++ src/main/java/com/akash/agentic_honey_pot/filter/RequestLoggingFilter.java
@@ -0,0 +1,30 @@
+package com.akash.agentic_honey_pot.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request,0);
+        filterChain.doFilter(wrappedRequest, response);
+        byte[] body = wrappedRequest.getContentAsByteArray();
+        if(body.length > 0){
+            String requestBody = new String(body, StandardCharsets.UTF_8);
+            log.info("RAW REQUEST BODY: {}", requestBody);
+        }
+    }
+}
